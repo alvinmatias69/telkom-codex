c4d6112d721b3f87be0f9e7b8995dfe20f54dcff
Helmi Satria Nugraha <satriahelmi@gmail.com>
2018-09-21 21:17:26 +0700

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
-import app from './app';
+import app from "./app";
+import cors from "cors";
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.listen(PORT, () => console.log(`Express app listening on port ${PORT}`));
